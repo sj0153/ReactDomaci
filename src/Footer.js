7d014afdb93a176ec6eadc48a5224c787bc599a8
sj0153 <fiste.ooo@gmail.com>
2022-06-24 15:50:23 +0200

    v1.4
    dodat footer

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import './style/Footer.css'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <h6>Internet tehnologije: treći domaći</h6>
+            </div>
+        )
+    }
+}
