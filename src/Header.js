a521a6ac8e6ae17624cada2b63af53dd42dea586
sj0153 <fiste.ooo@gmail.com>
2022-06-25 22:24:09 +0200

    dizajn

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
     render() {
         return (
             <div className="header">
-                <a className="meni_link" href="/" ><h1>Mastermind</h1></a>
+                <a className="meni_link" href="/" ><h1 >Mastermind</h1></a>
                 <a className="meni_link" href="/igra">Nova igra</a>
                 <a className="meni_link"  href="/pravila">Pravila</a>
             </div>
