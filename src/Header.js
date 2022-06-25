ce9221b56be96fed01a4e2060d21f56e38d82fa8
sj0153 <fiste.ooo@gmail.com>
2022-06-25 23:47:50 +0200

    dodat link za bolje performanse

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom'
 import './style/Header.css'
 
 export default class Header extends Component {
     render() {
         return (
             <div className="header">
-                <a className="meni_link" href="/" ><h1 >Mastermind</h1></a>
-                <a className="meni_link" href="/igra">Nova igra</a>
-                <a className="meni_link"  href="/pravila">Pravila</a>
+                <Link className="meni_link" to="/" ><h1 >Mastermind</h1></Link>
+                <Link className="meni_link" to="/igra">Nova igra</Link>
+                <Link className="meni_link"  to="/pravila">Pravila</Link>
             </div>
         )
     }
